1fc6b1e4ea2c4a62259d6d59c60aff5c9a5f769e
salaheddinechehbi <chehbi.se@gmail.com>
2020-05-03 00:15:00 +0000

    # WARNING: head commit changed in the meantime
    fff

---
+++ src/main/java/com/sec/ma/config/SeurityConfig.java
@@ -0,0 +1,64 @@
+package com.sec.ma.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SeurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//here when we add the allowed user to authenticate to our application
+		//here we create users in memory not from database
+		
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("{noop}1235")
+			.roles("USER")
+		.and()
+			.withUser("admin")
+			.password("{noop}1236")
+			.roles("ADMIN");
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		/*http.authorizeRequests()
+			.antMatchers("/**").hasRole("ADMIN")//here we give the access only to user with role ADMIN and he have access to all url path with /**
+			.and()
+			.formLogin();*/
+		
+		/*http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")//the admin path should be accessing only to ADMIN user
+		.antMatchers("/user").hasRole("USER")//the user path should be accessing only to USER user
+		.antMatchers("/","static/css","static/js").permitAll()//we this we tell spring to return this pages however is user login or not
+		.and()
+		.formLogin();*/
+		
+		
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")//the admin path should be accessing only to ADMIN user
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")//the user path should be accessing only to USER user
+		.antMatchers("/","static/css","static/js").permitAll()//we this we tell spring to return this pages however is user login or not, or we use only /
+		.and()
+		.formLogin();
+		
+		
+		//we have to respect the order of heiher role to lower
+	}
+	
+	
+	
+	
+
+	
+}
