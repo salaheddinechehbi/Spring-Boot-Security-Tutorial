6208dbacabe050ef4b0f4aeb410f162e1576780e
salaheddinechehbi <chehbi.se@gmail.com>
2020-05-15 18:25:01 +0000

    Add User Service And Form

--- src/main/java/com/sec/ma/controller/UserController.java
+++ src/main/java/com/sec/ma/controller/UserController.java
@@ -41,13 +41,25 @@ public class UserController {
 	@GetMapping("/userDetails")
 	public ModelAndView findAllUser() {
 		ModelAndView modelAndView = new ModelAndView();
-		//PrintStream out = System.out;System.setOut(out);
-		//System.out.println("*********************************************************************************");
-		//System.out.println(userService.findAll());
-		//System.out.println("*********************************************************************************");
-		modelAndView.addObject("successMes", "Please correct");
 		modelAndView.addObject("listUsers", userService.findAll());
-		//modelMap.addAttribute("list", userService.findAll());
+		modelAndView.addObject("user", new User());
+		modelAndView.setViewName("userDetails");
+		return modelAndView;
+	}
+	
+	@PostMapping("/userDetails")
+	public ModelAndView registerU(@Valid User user, BindingResult bindingResult, ModelMap modelMap) {
+		ModelAndView modelAndView = new ModelAndView();
+		// Check for the validations
+		if(bindingResult.hasErrors()) {
+			modelAndView.addObject("successMessage", "Please correct the errors in form!");
+			modelMap.addAttribute("bindingResult", bindingResult);
+		}else {
+			userService.save(user);
+			modelAndView.addObject("successMessage", "User is registered successfully!");
+		}
+		modelAndView.addObject("user", new User());
+		modelAndView.addObject("listUsers", userService.findAll());
 		modelAndView.setViewName("userDetails");
 		return modelAndView;
 	}
